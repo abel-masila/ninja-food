4c54c4396e2f5ad63caf4545fe303a1d6f76e8e1
Abel Masila <abelmasila@gmail.com>
2021-01-06 02:31:12 +0300

    Added tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
